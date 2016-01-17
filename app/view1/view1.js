257f9d1a8e563f12b395547fbe9c89cc9c7a5aa4
lmnleaf <larkfleming@gmail.com>
2016-01-16 19:47:12 -0700

    initial commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,7 +1,11 @@
 'use strict';
 
+// gives access to a module ngRoute that we're using in our own module
+// creates a new module for view1
 angular.module('myApp.view1', ['ngRoute'])
 
+// .config allows declaring a controller without using ng-controller in the view
+// $routeProvider is a module - declare view template used when pointing to a particular path
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
@@ -9,6 +13,37 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', function(scope) {
 
+  var startingList = [
+    {
+      Note: {
+        title: "Note 1",
+        body: "Here is note 1."
+      },
+    },
+    {
+      Note: {
+        title: "Note 2",
+        body: "Here is note 2."
+      }
+    },
+    {
+      Note: {
+        title: "Note 3",
+        body: "Here is note 3."
+      }
+    }
+  ];
+
+  localStorage.setItem('notes', JSON.stringify(startingList)),
+
+  scope.notesList = JSON.parse(localStorage.getItem('notes')),
+
+  console.log(JSON.parse(localStorage.getItem('notes'))),
+
+  scope.addNote = function (note) {
+    console.log(scope.note);
+    localStorage.setItem('note', scope.note);
+  }
 }]);
