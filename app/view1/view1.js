f38382b4964815a494a42e0dc76196171973ff63
lmnleaf <larkfleming@gmail.com>
2016-01-16 20:17:14 -0700

    deletes note from notes list

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -36,12 +36,14 @@ angular.module('myApp.view1', ['ngRoute'])
     }
   ];
 
+// sets notesList in local storage
   localStorage.setItem('notes', JSON.stringify(startingList)),
 
   scope.notesList = JSON.parse(localStorage.getItem('notes')),
 
   console.log(JSON.parse(localStorage.getItem('notes'))),
 
+// adds a new note to the notesList in local storage
   scope.addNote = function (note) {
     localStorage.setItem('note', scope.note);
     scope.notesList.push({
@@ -50,8 +52,17 @@ angular.module('myApp.view1', ['ngRoute'])
         body: scope.noteBody
       }
     });
-    localStorage.setItem('notes', scope.NotesList);
+    localStorage.setItem('notes', scope.notesList);
     scope.noteTitle = '';
     scope.noteBody = '';
+  },
+
+// deletes a note from the notesList in local storage
+  scope.deleteNote = function (index) {
+    scope.notesList.splice(index);
+    localStorage.removeItem(index);
+    localStorage.setItem('notes', scope.notesList);
   }
+
+
 }]);
